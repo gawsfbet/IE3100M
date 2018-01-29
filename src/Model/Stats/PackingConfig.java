866d9ac75c35e102940cfafe17820de1a38f2313
elbatius <elbatius@gmail.com>
2018-01-29 16:49:01 +0800

    testing if this works.

--- src/Model/Stats/PackingConfig.java
+++ src/Model/Stats/PackingConfig.java
@@ -14,63 +14,67 @@ import Model.Product.Level3_Bin;
 public class PackingConfig /*implements Comparable<PackingConfig>*/ {
     private final Level3_Bin mainBin;
     private Level3_Bin lastBin;
-    
+
     private long totalEmptyVol;
     private final int emptyVolPerBin; //might not need
     private final int totalBoxesPerBin;
     private int totalBins;
     private final int remainderBoxes;
-    
+
     public PackingConfig (Level3_Bin mainBin, int totalBoxesPerBin, int totalBins, int emptyVolPerBin, int remainderBoxes) {
         this.mainBin = mainBin;
-        
+
         this.totalBoxesPerBin = totalBoxesPerBin;
         this.totalBins = totalBins;
         this.remainderBoxes = remainderBoxes;
         this.emptyVolPerBin = emptyVolPerBin;
         this.totalEmptyVol = (long) totalBins * emptyVolPerBin;
     }
-    
+
     public int getTotalBins() {
         return this.totalBins;
     }
-    
+
+    public int getEmptyVolPerBin(){
+      return this.emptyVolPerBin;
+    }
+
     public int getTotalBinsInclRemainder() {
         return remainderBoxes == 0 ? this.totalBins : this.totalBins + 1;
     }
-    
+
     public int getTotalBoxesPerBin() {
         return this.totalBoxesPerBin;
     }
-    
+
     public int getRemainderBoxes() {
         return this.remainderBoxes;
     }
-    
+
     public Level3_Bin getLastBin() {
         return this.lastBin;
     }
-    
+
     public long getTotalEmptyVol() {
         return this.totalEmptyVol;
     }
-    
+
     public void setLastBin(Level3_Bin lastBin) {
         this.lastBin = lastBin;
     }
-    
+
     public void setTotalBins(int totalBins) {
         this.totalBins = totalBins;
     }
-    
+
     public void setTotalEmptyVol(long totalEmptyVol) {
         this.totalEmptyVol = totalEmptyVol;
     }
-    
+
     /**
      * Modify this method if another attribute is to be minimised
      * @param other
-     * @return 
+     * @return
      */
     /*@Override
     public int compareTo(PackingConfig other) {
@@ -80,14 +84,14 @@ public class PackingConfig /*implements Comparable<PackingConfig>*/ {
             return this.getTotalBinsInclRemainder() - other.getTotalBinsInclRemainder();
         }
     }*/
-    
+
     @Override
     public String toString() {
-        String info = "Bin type: " + this.mainBin.toString() 
+        String info = "Bin type: " + this.mainBin.toString()
                 + "\nBoxes per Bin: " + totalBoxesPerBin
                 + "\nBins Needed: " + totalBins;
-        
+
         return info + (lastBin == null ? "\nLast Bin not needed" : "\n" + lastBin.toString() + " to contain remaining " + remainderBoxes + " boxes");
     }
-    
+
 }
--- src/ie3100m/Main.java
+++ src/ie3100m/Main.java
@@ -34,31 +34,39 @@ public class Main {
         int numOrderedBox = 1000;
         Level2_Box box = new Level2_Box(190, 186, 23, 0.65);
         Order order = new Order(box, numOrderedBox); //in mm and g
-        
+
         ArrayList<Level3_Bin> binList = new ArrayList<>();
         ArrayList<BinStats> allBinStats = new ArrayList<>();;
         ArrayList<PackingConfig> configs = new ArrayList<>();;
-        
+
         try {
             binList = FileUtils.loadBinTypes("boxes.csv");
         } catch (IOException ex) {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
         allBinStats = calculateStatsForAllBins(box, binList);
         configs = determineAllConfigs(order, allBinStats);
-        
-        PackingConfig bestConfig = determineBestConfig(configs);
-        
+
+        ArrayList<PackingConfig> binsByNumbers = new ArrayList<>();
+        ArrayList<PackingConfig> binsByVolume = new ArrayList<>();
+
+        binsByNumbers = rankByNumbers(configs);
+        binsByVolume = rankByVolume(configs);
+
+        PackingConfig bestConfig = determineBestConfig(binsByNumbers, binsByVolume);
+
+        // PackingConfig bestConfig = determineBestConfig(configs);
+
         if (bestConfig == null) {
             System.out.println("No suitable config found");
         } else {
             System.out.println("Chosen config:");
             System.out.println(bestConfig);
         }
-        
+
     }
-    
+
     /**
      * Determines the maximum number of the specified level 2 boxes that can fit into the given level 3 bin.
      * @param box The level 2 boxes to be packed
@@ -67,7 +75,7 @@ public class Main {
      */
     private static ArrayList<BinStats> calculateStatsForAllBins(Level2_Box box, ArrayList<Level3_Bin> binList) {
         ArrayList<BinStats> binStats = new ArrayList<>();
-        
+
         binList.stream().forEach((bin) -> {
             try {
                 binStats.add(calculateStatsForBin(box, bin));
@@ -75,32 +83,32 @@ public class Main {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
-        
+
         return binStats;
     }
-    
+
     /**
      * Determines the maximum number of the specified level 2 boxes that can fit into the given level 3 bin.
      * @param box The level 2 boxes to be packed
      * @param bin The level 3 bin to be used
      * @return the BinStats object for the maximum number of level 2 boxes that can fit
-     * @throws IloException 
+     * @throws IloException
      */
     private static BinStats calculateStatsForBin(Level2_Box box, Level3_Bin bin) throws IloException {
         Solver solver = new Solver(box, calcUpperBound(box, bin), bin);
         int quantityPerLayer = solver.optimize(false);
-        
+
         int totalQuantity = quantityPerLayer * (bin.getHeight() / box.getHeight());
-        
+
         if (totalQuantity * box.getWeight() > MAX_WEIGHT) {
             totalQuantity = (int) (MAX_WEIGHT / box.getWeight());
         }
-        
+
         int emptyVolume = (bin.getVolume()) - (totalQuantity * box.getVolume());
-        
+
         /*System.out.println("Number of boxes for bin type " + bin.toString() + ": " + totalQuantity);
         System.out.println("Empty space for bin type " + bin.toString() + ": " + emptyVolume);*/
-        
+
         return new BinStats(bin, quantityPerLayer, totalQuantity, emptyVolume);
     }
 
@@ -108,21 +116,21 @@ public class Main {
      * Given COMPLETE list of bins, calculate config for each bin if it were the main bin
      * @param order
      * @param allBinStats
-     * @return 
+     * @return
      */
     private static ArrayList<PackingConfig> determineAllConfigs(Order order, ArrayList<BinStats> allBinStats) {
         ArrayList<PackingConfig> packingConfigs = new ArrayList<>();
-        
+
         allBinStats.stream().forEach((binStat) -> {
             PackingConfig config = determineConfig(order, binStat, allBinStats);
             if (config != null) {
                 packingConfigs.add(config);
             }
         });
-        
+
         return packingConfigs;
     }
-    
+
     /**
      * Calculates the number of bins to use for the given order, as well as determine the remaining bin to use (if needed)
      * @param order The order of level 2 boxes and the quantity to be packed
@@ -132,36 +140,36 @@ public class Main {
      */
     private static PackingConfig determineConfig(Order order, BinStats binStats, ArrayList<BinStats> candidateRemainingBins) {
         Level2_Box box = order.getBox();
-        
+
         PackingConfig config = null;
-        
+
         if (order.getQuantity() < binStats.getTotalQuantity()) { //one box can fit everything
             int emptyVolPerBin = binStats.getBin().getVolume() - (order.getQuantity() * box.getVolume());
-            config = new PackingConfig(binStats.getBin(), 
-                    order.getQuantity(), 
-                    1, 
-                    emptyVolPerBin, 
+            config = new PackingConfig(binStats.getBin(),
+                    order.getQuantity(),
+                    1,
+                    emptyVolPerBin,
                     0);
         } else if (binStats.getTotalQuantity() == 0) { //level 2 item cannot fit in the bin
             //do nothing
         } else {
             int remainingBoxes = order.getQuantity() % binStats.getTotalQuantity();
-            
-            config = new PackingConfig(binStats.getBin(), 
-                    binStats.getTotalQuantity(), 
-                    order.getQuantity() / binStats.getTotalQuantity(), 
-                    binStats.getEmptyVolume(), 
+
+            config = new PackingConfig(binStats.getBin(),
+                    binStats.getTotalQuantity(),
+                    order.getQuantity() / binStats.getTotalQuantity(),
+                    binStats.getEmptyVolume(),
                     remainingBoxes);
-            
+
             if (config.getRemainderBoxes() != 0) {
                 config.setLastBin(determineRemainderBin(box, config.getRemainderBoxes(), candidateRemainingBins));
                 config.setTotalEmptyVol(config.getTotalEmptyVol() + (config.getLastBin().getVolume() - box.getVolume() * config.getRemainderBoxes()));
             }
         }
-        
+
         return config;
     }
-    
+
     /**
      * Determines the hest bin to use for the remaining boxes
      * @param box The level 2 boxes to be packed
@@ -172,7 +180,7 @@ public class Main {
     private static Level3_Bin determineRemainderBin(Level2_Box box, int remainder, ArrayList<BinStats> candidateRemainingBins) {
         Level3_Bin lastBin = null;
         int emptyVol, minEmptyVol = Integer.MAX_VALUE;
-        
+
         for (BinStats binStat : candidateRemainingBins) {
             if (binStat.getTotalQuantity() >= remainder) { //if the bin can contain all the remainder boxes
                 emptyVol = binStat.getBin().getVolume() - box.getVolume() * remainder;
@@ -182,16 +190,27 @@ public class Main {
                 }
             }
         }
-        
+
         return lastBin;
     }
-    
+
     /**
      * Determine the best packing configuration based on the sorting order
      * @param packingConfigs the array of possible packing configurations
      * @return the most desired packing configuration
      */
-    private static PackingConfig determineBestConfig(ArrayList<PackingConfig> packingConfigs) {
+     private static ArrayList<PackingConfig> determineBestConfig(ArrayList<PackingConfig> binsByNumbers, ArrayList<PackingConfig> binsByVolume){
+        PackingConfig firstRank = null;
+        for (PackingConfig numRank : binsByNumbers) {
+          
+        }
+
+     }
+
+
+
+
+    private static ArrayList<PackingConfig> rankByNumbers(ArrayList<PackingConfig> packingConfigs) {
         //might use a PQ for this instead
         Collections.sort(packingConfigs, (a, b) -> {
             if (a.getTotalBinsInclRemainder() == b.getTotalBinsInclRemainder()) {
@@ -200,14 +219,25 @@ public class Main {
                 return a.getTotalBinsInclRemainder() - b.getTotalBinsInclRemainder();
             }
         });
-        
-        for (PackingConfig config : packingConfigs) {
-            if (config.getTotalBoxesPerBin() != 1) {
-                return config;
+
+        // for (PackingConfig config : packingConfigs) {
+        //     if (config.getTotalBoxesPerBin() != 1) {
+        //         return config;
+        //     }
+        // }
+
+        return packingConfigs;
+    }
+
+    private static ArrayList<PackingConfig> rankByVolume(ArrayList<PackingConfig> packingConfigs){
+        Collections.sort(packingConfigs, (a, b) -> {
+            if (a.getEmptyVolPerBin() == b.getEmptyVolPerBin()) {
+                return a.getTotalBinsInclRemainder() <= b.getTotalBinsInclRemainder() ? -1 : 1;
+            } else {
+                return a.getEmptyVolPerBin() - b.getEmptyVolPerBin();
             }
-        }
-        
-        return null;
+        });
+        return packingConfigs;
     }
 
     private static int calcUpperBound(Level2_Box box, Level3_Bin bin) {
